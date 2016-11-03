c396e67b0f5af8741478f5896de47e85c67ec234
Chris Bay <chris@chrisbay.net>
2016-11-03 13:21:31 -0500

    Add stashed changes

--- src/main/java/org/launchcode/controllers/HelloController.java
+++ src/main/java/org/launchcode/controllers/HelloController.java
@@ -2,7 +2,6 @@ package org.launchcode.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.launchcode.models.HelloMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +9,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloController {
-
+	
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	public String helloform(HttpServletRequest request) {
+	public String helloForm() {
 		return "helloform";
 	}
-	
+
 	@RequestMapping(value = "/hello", method = RequestMethod.POST)
 	public String hello(HttpServletRequest request, Model model) {
 		
