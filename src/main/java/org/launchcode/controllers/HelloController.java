f7ea3cbc43ed1f96758cdef7fec71f2714347a84
Chris Bay <chris@chrisbay.net>
2016-11-03 11:39:39 -0500

    Add most or all of walkthrough 2 code

--- src/main/java/org/launchcode/controllers/HelloController.java
+++ src/main/java/org/launchcode/controllers/HelloController.java
@@ -4,25 +4,29 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.launchcode.models.HelloMessage;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	@ResponseBody
-	public String hello(HttpServletRequest request) {
+	public String helloform(HttpServletRequest request) {
+		return "helloform";
+	}
+	
+	@RequestMapping(value = "/hello", method = RequestMethod.POST)
+	public String hello(HttpServletRequest request, Model model) {
 		
-		// get name parameter from request; will be null of no parameter passed in
 		String name = request.getParameter("name");
 		
-		if (name == null) {
+		if (name == null || name == "") {
 			name = "world";
 		}
 		
-		return "<h1>" + HelloMessage.getMessage(name) + "</h1>";
+		model.addAttribute("name", name);
+		return "hello";
 	}
 	
 }
